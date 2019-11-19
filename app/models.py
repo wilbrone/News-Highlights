4a2a28bb36d6625372ffadab75fbede725cf50fb
wilbrone <wilbroneokoth@gmail.com>
2019-11-19 19:22:12 +0300

    add tests

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class Articles:
     '''
     Articles class to define articles objects
     '''
+    
     def __init__(self,id,author,title,description,url,image,date):
         self.id = id
         self.author = author
